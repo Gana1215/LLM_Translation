cec0f94e4cdf45d05b839b630c7d392486fae7b4
Gana1215 <ganaacts@gmail.com>
2025-09-30 16:41:17 -0700

    Update LLM_Translator.py

--- LLM_Translator.py
+++ LLM_Translator.py
@@ -38,7 +38,7 @@ load_css("style.css", "record.css")
 genai.configure(api_key="AIzaSyBMj0Yshu5o4YxMp2oLImlseU6lV_FiFjI")  # Replace with your key
 gen_model = genai.GenerativeModel("gemini-2.0-flash")
 # INITIALIZE LFS CLIENT                                  
-lfs_client = git_lfs.GitLFSClient(str(model_dir))
+#lfs_client = git_lfs.GitLFSClient(str(model_dir))
 # 
 # ----------------- Whisper MN-SP-MINI ASR -----------------
 @st.cache_resource
@@ -55,6 +55,7 @@ def load_mongolian_asr():
 
         # Detect LFS pointer and download real content
         if content.startswith("version https://git-lfs.github.com/spec/v1"):
+            lfs_client = git_lfs.GitLFSClient(str(model_dir))
             pointer = lfs_client.get_pointer(str(config_file))
             content = lfs_client.download_file(pointer).decode("utf-8")
 
